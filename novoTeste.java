41387b276fb8f8464c2069f66d63f572cc59a948
WilliamDullius <william_dullius@estudante.sesisenai.org.br>
2022-10-10 16:48:29 -0300

    Testee

---
+++ novoTeste.java
@@ -0,0 +1,11 @@
+public class aula1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World odhsioadhaihdas - UHUUULLL");
+		System.out.println("Hello Wsdoahdiaoshorld - UHUUULLL");
+		System.out.println("Hello World - UHUUsadjoiaULLL");
+
+	}
+
+}
+
